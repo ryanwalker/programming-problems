c24e2230397a3d7139bf297e622e11de45cc20b8
Ryan Walker <ryan.walker@infusionsoft.com>
2018-08-15 14:30:55 -0700

    palnindtrome checker

---
+++ src/test/java/com/ryanwalker/strings/PalindromeTest.java
@@ -0,0 +1,35 @@
+package com.ryanwalker.strings;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PalindromeTest {
+
+  public static void main(String[] args) {
+
+    List<String> potentialPalindromes = Arrays.asList("cow cat", "tacocat", "taco cat", "s", "aa", "aaa", "");
+
+    for (String potentialPalindrome : potentialPalindromes) {
+      System.out.println(isPalindrome(potentialPalindrome));
+    }
+  }
+
+  public static String isPalindrome(String candidate) {
+    if (candidate == null || candidate.length() == 0) {
+      return "invalid string";
+    }
+
+    int beginCursor = 0;
+    int endCursor = candidate.length() - 1;
+
+    while (beginCursor < endCursor) {
+      if (candidate.charAt(beginCursor) != candidate.charAt(endCursor)) {
+        return candidate + " is NOT a palindrome";
+      }
+//TODO - solve whitespace bug
+      beginCursor++;
+      endCursor--;
+    }
+    return candidate + " IS a palindrome";
+  }
+}
