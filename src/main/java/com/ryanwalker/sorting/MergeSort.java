0f873641b5c1dfe011fa4b431f455acb5a0536ba
Ryan Walker <ryan.walker@infusionsoft.com>
2018-09-13 12:02:22 -0700

    MergeSort list working

--- src/main/java/com/ryanwalker/sorting/MergeSort.java
+++ src/main/java/com/ryanwalker/sorting/MergeSort.java
@@ -1,10 +1,11 @@
 package com.ryanwalker.sorting;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MergeSort {
-  public static List<Integer> sort(List<Integer> list) {
+  public static List<Integer> sortList(List<Integer> list) {
     int listSize = list.size();
 
     switch (listSize) {
@@ -20,9 +21,62 @@ public class MergeSort {
           return list;
         }
       default:
-        break;
-    }
+        //Split the list into two
+        int endIndex = list.size() - 1;
+        int middleIndex = (listSize / 2) + (listSize % 2);
+        List<Integer> leftList = list.subList(0, middleIndex);
+        List<Integer> rightList = list.subList(middleIndex, listSize);
 
+        //recursive call to sort left and right list
+        List<Integer> sortedLeft = sortList(leftList);
+        List<Integer> sortedRight = sortList(rightList);
+
+        //merge them back together
+        List<Integer> mergedList = merge(sortedLeft, sortedRight);
+        return mergedList;
+    }
+  }
+
+  private static List<Integer> merge(List<Integer> sortedLeft, List<Integer> sortedRight) {
+    List<Integer> mergedList = new ArrayList<>();
+    int totalElements = sortedLeft.size() + sortedRight.size();
+
+    int leftIndex = 0;
+    int leftEndIndex = sortedLeft.size();
+    int rightIndex = 0;
+    int rightEndIndex = sortedRight.size();
+
+    while (totalElements > 0) {
+      Integer leftVal = null;
+      Integer rightVal = null;
+      if (leftIndex < leftEndIndex) {
+        leftVal = sortedLeft.get(leftIndex);
+      }
+      if (rightIndex < rightEndIndex) {
+        rightVal = sortedRight.get(rightIndex);
+      }
+
+      if (leftVal != null && rightVal != null) {
+        if (leftVal > rightVal) {
+          mergedList.add(rightVal);
+          rightIndex++;
+        } else {
+          mergedList.add(leftVal);
+          leftIndex++;
+        }
+      } else if (rightVal == null) {
+        mergedList.add(leftVal);
+        leftIndex++;
+      } else {
+        mergedList.add(rightVal);
+        rightIndex++;
+      }
+      totalElements--;
+    }
+    return mergedList;
+  }
+
+  public static int[] sortArray() {
     return null;
   }
 }
--- src/test/java/com/ryanwalker/sorting/MergeSortTest.java
+++ src/test/java/com/ryanwalker/sorting/MergeSortTest.java
@@ -11,17 +11,49 @@ public class MergeSortTest {
   @Test
   public void testMergeSort1Item() {
     List<Integer> list = Arrays.asList(1);
-    List<Integer> sortedList = MergeSort.sort(list);
+    List<Integer> sortedList = MergeSort.sortList(list);
 
     Assertions.assertEquals(list, sortedList);
 
   }
 
   @Test
-  public void testMergeSort2Item() {
+  public void testMergeSort2Items() {
     List<Integer> list = Arrays.asList(3, 1);
-    List<Integer> sortedList = MergeSort.sort(list);
+    List<Integer> sortedList = MergeSort.sortList(list);
 
     Assertions.assertEquals(Arrays.asList(1, 3), sortedList);
+  }
+
+  @Test
+  public void testMergeSort2ItemsInOrder() {
+    List<Integer> list = Arrays.asList(1, 3);
+    List<Integer> sortedList = MergeSort.sortList(list);
+
+    Assertions.assertEquals(Arrays.asList(1, 3), sortedList);
+  }
+
+  @Test
+  public void testMergeSort3Item() {
+    List<Integer> list = Arrays.asList(3, 1, 2);
+    List<Integer> sortedList = MergeSort.sortList(list);
+
+    Assertions.assertEquals(Arrays.asList(1, 2, 3), sortedList);
+  }
+
+  @Test
+  public void testMergeSort4Item() {
+    List<Integer> list = Arrays.asList(7, 3, 1, 2);
+    List<Integer> sortedList = MergeSort.sortList(list);
+
+    Assertions.assertEquals(Arrays.asList(1, 2, 3, 7), sortedList);
+  }
+
+  @Test
+  public void testMergeSort10Item() {
+    List<Integer> list = Arrays.asList(82, 3, 3, 1, 2, 99, 1, 8, 46);
+    List<Integer> sortedList = MergeSort.sortList(list);
+
+    Assertions.assertEquals(Arrays.asList(1, 1, 2, 3, 3, 8, 46, 82, 99), sortedList);
   }
 }
