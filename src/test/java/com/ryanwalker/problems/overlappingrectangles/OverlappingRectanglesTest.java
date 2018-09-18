5383a3d55c0503b92491d784431848acb4047ede
Ryan Walker <ryan.walker@infusionsoft.com>
2018-09-18 08:19:55 -0700

    Test complete overlap

--- src/test/java/com/ryanwalker/problems/overlappingrectangles/OverlappingRectanglesTest.java
+++ src/test/java/com/ryanwalker/problems/overlappingrectangles/OverlappingRectanglesTest.java
@@ -45,6 +45,61 @@ public class OverlappingRectanglesTest {
   }
 
   @Test
-  public void testNoYOverlap() {
+  public void testNoOverlap() {
+    OverlappingRectangles overlap = new OverlappingRectangles();
+
+    Rectangle rectangleA = new Rectangle()
+        .withCoordinate(1,3)
+        .withCoordinate(1,4)
+        .withCoordinate(4,3)
+        .withCoordinate(4,4);
+
+    Rectangle rectangleB = new Rectangle()
+        .withCoordinate(2,1)
+        .withCoordinate(2,2)
+        .withCoordinate(3,1)
+        .withCoordinate(3,2);
+
+    overlap.coordinatesOfOverlap(rectangleA, rectangleB);
+
+  }
+
+  @Test
+  public void testOverlap() {
+    OverlappingRectangles overlap = new OverlappingRectangles();
+
+    Rectangle rectangleA = new Rectangle()
+        .withCoordinate(1,3)
+        .withCoordinate(1,4)
+        .withCoordinate(4,3)
+        .withCoordinate(4,4);
+
+    Rectangle rectangleB = new Rectangle()
+        .withCoordinate(2,1)
+        .withCoordinate(2,4)
+        .withCoordinate(3,1)
+        .withCoordinate(3,4);
+
+    overlap.coordinatesOfOverlap(rectangleA, rectangleB);
+
+  }
+
+  @Test
+  public void testCompleteOverlap() {
+    OverlappingRectangles overlap = new OverlappingRectangles();
+
+    Rectangle rectangleA = new Rectangle()
+        .withCoordinate(1,1)
+        .withCoordinate(1,3)
+        .withCoordinate(3,1)
+        .withCoordinate(3,3);
+
+    Rectangle rectangleB = new Rectangle()
+        .withCoordinate(2,1)
+        .withCoordinate(2,2)
+        .withCoordinate(3,1)
+        .withCoordinate(3,2);
+
+    Rectangle rectangle = overlap.coordinatesOfOverlap(rectangleA, rectangleB);
   }
 }
